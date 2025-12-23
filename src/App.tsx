a79e98604a75ce4472498b1b1c47006922ef8db0
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-23 14:58:33 -0500

    elementos al state de order

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-
+import MenuItem from "./components/MenuItem"
+import { menuItems } from "./data/ds"
+import useOrder from "./hooks/useOrder"
 
 function App() {
+  const { addItem } = useOrder()
 
   return (
     <>
-      <h1>Hola mundo!</h1>
+      <header className="bg-emerald-800 py-5">
+        <h1 className="text-center text-4xl font-black text-white">Calculadora de Propinas y Consumos</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="py-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))} 
+          </div>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,19 @@
+
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+  item: MenuItem,
+  addItem: (item: MenuItem) => void
+}
+
+export default function MenuItem({item, addItem} : MenuItemProps) {
+  return (
+    <button
+    className="border-2 border-emerald-500 hover:bg-emerald-300 w-full p-3 flex justify-between"
+    onClick={() => addItem(item)}
+    >
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
+  )
+}
